279d8388d068d1fc5282b1ddf0006249a9734b93
Brian Martin Icochea Lopez <brian.lopez@gamigo.com>
2020-12-08 15:26:28 +0100

    Changed files

--- python_testing/home_page/steps/demo_page.py
+++ python_testing/home_page/steps/demo_page.py
@@ -6,9 +6,10 @@ from nose.tools import assert_equal
 @given("the demo website")
 def website_navigation(context):
     url = 'https://phptravels.com/demo/'
-    context.browser = webdriver.Chrome('/home/ibrian93/chromedriver.exe')
+    context.browser = webdriver.Chrome()
     context.browser.get(url)
-
+    import time
+    time.sleep(10)
 
 @given("the title is available in home")
 def assert_title(context):
