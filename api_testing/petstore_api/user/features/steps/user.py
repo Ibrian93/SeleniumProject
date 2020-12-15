ab46ac6abaabcaf2f2cdf1b2bf0aaeee7fe2b984
Brian Martin Icochea Lopez <Ibrian93@gmail.com>
2020-12-15 22:35:59 +0100

    Structure finalized

--- api_testing/petstore_api/user/features/steps/user.py
+++ api_testing/petstore_api/user/features/steps/user.py
@@ -2,6 +2,7 @@ import requests
 from api_testing.petstore_api.user.models.user_model import User
 from behave import given, when, then
 
+
 @given("a user with the following data:")
 def step_impl(context):
     for row in context.table:
