422daa4ed7bf2d1b718a4f01954b4900cc095215
Brian Martin Icochea Lopez <Ibrian93@gmail.com>
2021-01-07 12:56:10 +0100

    Added match post body and get response

--- api_testing/petstore_api/user/features/create_user.feature
+++ api_testing/petstore_api/user/features/create_user.feature
@@ -16,3 +16,13 @@ Scenario: A user is created and its information is retrieved correctly
     And it is created the user
     When the user information is retrieved
     Then the information is retrieved correcly
+
+
+Scenario: The user information retrieved matches the user created account
+    Given a user with the following data:
+	    | username | firstname | lastname | email	   | password | phone      |
+	    |  brian   |  brian    | icochea  | test@test.com  | testing  | 123 456789 |
+    And the user is active
+    And it is created the user
+    When the user information is retrieved
+    Then the information retrieved matches with the created user
--- api_testing/petstore_api/user/features/steps/user.py
+++ api_testing/petstore_api/user/features/steps/user.py
@@ -15,6 +15,7 @@ def step_impl(context):
                             email=row["email"],
                             password=row["password"],
                             phone=row["phone"])
+
 @given('the user is active')
 def step_impl(context):
     context.user.status = 1
@@ -24,7 +25,7 @@ def step_impl(context):
 def step_impl(context):
     endpoint = 'user'
     header = {'accept': 'application/json', 'Content-type': 'application/json'}
-    body = {'id': context.user.id,
+    context.body = {'id': context.user.id,
             'username': context.user.username,
             'firstname': context.user.firstname,
             'lastname': context.user.lastname,
@@ -32,11 +33,11 @@ def step_impl(context):
             'password': context.user.password,
             'phone': context.user.phone,
             'userStatus': context.user.status}
-    context.r = requests.post(BASE_URL + endpoint, headers=header, data=json.dumps(body))
+    context.r = requests.post(BASE_URL + endpoint, headers=header, data=json.dumps(context.body))
 
 @when('the user information is retrieved')
 def step_impl(context): 
-    context.res = requests.get(BASE_URL + 'user/' + 2 context.user.username)
+    context.res = requests.get(BASE_URL + 'user/' + context.user.username)
 
 @then('the information is retrieved correcly')
 def step_impl(context):
@@ -46,3 +47,12 @@ def step_impl(context):
 @then('the user is created correctly')
 def step_impl(context):
     assert context.r.status_code == 200
+
+@then('the information retrieved matches with the created user')
+def step_impl(context):
+    response_info = context.res.text
+    
+    json_info = json.loads(response_info)
+    for key in json_info:
+        assert json_info[key] == context.body[key]
+    
