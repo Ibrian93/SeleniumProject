928da5be9472f7d381604bbbd78597612528e086
Brian Icochea <ibrian93@gmail.com>
2021-04-10 22:40:45 +0200

    adding first step to registration

---
+++ Frontend/Pages/login_page.py
@@ -0,0 +1,17 @@
+from Frontend.Pages.registration_page.py import RegistrationPage
+
+
+class LoginPage:
+
+    login_button = "login"
+    registration_button = "newUser"
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def go_to_registration_page(self):
+        self.driver.find_element_by_id(self.registration_button).click()
+        return RegistrationPage(self.driver)
+
+    def login_user(self):
+        self.driver.find_element_by_id(self.login_button).click()
---
+++ Frontend/Pages/registration_page.py
@@ -0,0 +1,18 @@
+import Frontend.Pages.LoginPage
+
+
+class RegistrationPage:
+
+    register_button = "register"
+    back_to_login_button = "gotologin"
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def register_user(self):
+        self.driver.find_element_by_id(self.register_button).click()
+        #check where it leads
+
+    def back_to_login_page(self):
+        self.driver.find_element_by_id(self.back_to_login_button).click()
+        return Frontend.Pages.LoginPage(self.driver)
