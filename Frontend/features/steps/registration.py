341f2ec216baef9f22a5fea1c2d5c03aadc60714
Brian Icochea <ibrian93@gmail.com>
2021-05-02 15:17:17 +0200

    adding dockers and working fine

--- Frontend/features/steps/registration.py
+++ Frontend/features/steps/registration.py
@@ -5,3 +5,23 @@ import features.steps.common
 @given('the user goes to the Login Page')
 def step_impl(context):
     pass
+
+@given('the user goes to the New User Page')
+def step_impl(context):
+    pass
+
+@given('the user introduces the following data:')
+def step_impl(context):
+    pass
+
+@given('the user validates the recaptcha')
+def step_impl(context):
+    pass
+
+@given('the user clicks on "Register"')
+def step_impl(context):
+    pass
+
+@given('the user should be registered')
+def step_impl(context):
+    pass
