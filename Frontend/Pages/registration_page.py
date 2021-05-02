d2c9fc43630ad9d6adc3ba8092d49dd65f7c44e3
Brian Icochea <ibrian93@gmail.com>
2021-05-02 21:09:53 +0200

    registration for a user - missing recaptcha

--- Frontend/Pages/registration_page.py
+++ Frontend/Pages/registration_page.py
@@ -5,6 +5,11 @@ class RegistrationPage:
 
     register_button = "register"
     back_to_login_button = "gotologin"
+    first_name_input = "firstname"
+    last_name_input = "lastname"
+    username_input = "userName"
+    password_input = "password"
+
 
     def __init__(self, driver):
         self.driver = driver
@@ -12,6 +17,24 @@ class RegistrationPage:
     def register_user(self):
         self.driver.find_element_by_id(self.register_button).click()
         #check where it leads
+
+    def set_first_name(self, first_name):
+        self.driver.find_element_by_id(self.first_name_input).send_keys(first_name)
+
+    def set_last_name(self, last_name):
+        self.driver.find_element_by_id(self.last_name_input).send_keys(last_name)
+
+    def set_username(self, username):
+        self.driver.find_element_by_id(self.username_input).send_keys(username)
+
+    def set_password(self, password):
+        self.driver.find_element_by_id(self.password_input).send_keys(password)
+
+    def input_registration_form(self, first_name, last_name, username, password):
+        self.set_first_name(first_name)
+        self.set_last_name(last_name)
+        self.set_username(username)
+        self.set_password(password)
 
     def back_to_login_page(self):
         self.driver.find_element_by_id(self.back_to_login_button).click()
--- Frontend/features/steps/registration.py
+++ Frontend/features/steps/registration.py
@@ -1,4 +1,5 @@
 from behave import given, when, then
+from Models.user import User
 import features.steps.common
 
 
@@ -12,12 +13,20 @@ def step_impl(context):
 
 @given('the user introduces the following data')
 def step_impl(context):
-    import time
-    time.sleep(30)
+    for row in context.table:
+        context.user = User(first_name=row["First Name"],
+                            last_name=row["Last Name"],
+                            user_name=row["UserName"],
+                            password=row["Password"])
+    context.registration_page.input_registration_form(first_name=context.user.first_name,
+                                                      last_name=context.user.last_name,
+                                                      username=context.user.user_name,
+                                                      password=context.user.password)
 
 @given('the user validates the recaptcha')
 def step_impl(context):
-    pass
+    import time
+    time.sleep(5)
 
 @when('the user clicks on "Register"')
 def step_impl(context):
