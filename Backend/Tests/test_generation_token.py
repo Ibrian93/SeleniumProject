7e054bb860619023e1242c124bdb9578c681abfd
Brian Icochea <ibrian93@gmail.com>
2021-05-13 18:01:14 +0200

    adding more assertions - model validation

--- Backend/Tests/test_generation_token.py
+++ Backend/Tests/test_generation_token.py
@@ -9,3 +9,8 @@ class TestClass:
         req = self.account_service.post_generate_token(data)
         assert req.status_code == 200
         assert req.json() is not None
+        assert isinstance(req.json()["token"], str)
+        assert isinstance(req.json()["expires"], str)
+        assert isinstance(req.json()["status"], str)
+        assert isinstance(req.json()["result"], str)
+        assert req.json()["status"] == "Success" and req.json()["result"] == "User authorized successfully."
