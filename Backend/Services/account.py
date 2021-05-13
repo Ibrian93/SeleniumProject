523ac5af7c0fc76e06db5a85564f4568864cbf48
Brian Icochea <ibrian93@gmail.com>
2021-05-13 17:05:02 +0200

    adding first test

--- Backend/Services/account.py
+++ Backend/Services/account.py
@@ -5,7 +5,7 @@ class Account:
 
     host = "https://demoqa.com"
 
-    def post_generate_token(self, data, headers):
+    def post_generate_token(self, data, headers=None):
         endpoint = "/Account/v1/GenerateToken"
         r = requests.post(url=self.host + endpoint, data=data, headers=headers)
         return r
--- Backend/Tests/test_generation_token.py
+++ Backend/Tests/test_generation_token.py
@@ -4,9 +4,8 @@ class TestClass:
 
     account_service = Account()
 
-    def post_generation_token_successful(self):
+    def test_post_generation_token_successful(self):
         data = {"userName": "ibrian93", "password": "MyTesting83!"}
-        headers = {"accept": "application/json", "Content-Type": "application/json"}
-        req = account_service.post_generate_token(data, headers)
+        req = self.account_service.post_generate_token(data)
         assert req.status_code == 200
         assert req.json() is not None
