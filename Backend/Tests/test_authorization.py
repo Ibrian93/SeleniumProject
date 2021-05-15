50c292b6d44a662d957bfd6b84cc398adc70570f
Brian Icochea <ibrian93@gmail.com>
2021-05-15 13:09:25 +0200

    Adding missing password test case

--- Backend/Tests/test_authorization.py
+++ Backend/Tests/test_authorization.py
@@ -12,8 +12,17 @@ class TestClass:
         assert isinstance(req.json(), bool)
         assert req.json() is True
 
-    def test_post_non_authorized_user_invalid_username(self):
+    def test_post_non_authorized_user_missing_username(self):
         data = {"userName": None, "password": "MyTesting83!"}
+        req = self.account_service.post_authorized(data)
+        assert req.status_code == 400
+        assert req.json() is not None
+        assert req.json()["code"] == "1200", "The code returned was: " + req.json()["code"]
+        assert req.json()["message"] == "UserName and Password required.", "The message returned was: " + req.json()["message"]
+
+
+    def test_post_non_authorized_user_missing_password(self):
+        data = {"userName": "ibrian93", "password": None}
         req = self.account_service.post_authorized(data)
         assert req.status_code == 400
         assert req.json() is not None
