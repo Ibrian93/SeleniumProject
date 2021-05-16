8c13d4f1bb9dfcc9b0f1d030146e1530abb2e65b
Brian Icochea <ibrian93@gmail.com>
2021-05-16 19:05:00 +0200

    changing dockers to catch environment variables

--- Backend/Tests/test_authorization.py
+++ Backend/Tests/test_authorization.py
@@ -52,7 +52,7 @@ class TestClass:
     def test_post_created_user_but_no_generated_token(self, random_user):
         data = Payload(username=random_user.username, password=random_user.password).to_dict()
         req = self.account_service.post_authorized(data)
-        assert req.status_code == 404
+        assert req.status_code == 200
         assert req.json() is not None
-        assert req.json()["code"] == "1207", "The code returned was: " + req.json()["code"]
-        assert req.json()["message"] == "User not found!", "The message returned was: " + req.json()["message"]
+        assert isinstance(req.json(), bool)
+        assert req.json() is False
