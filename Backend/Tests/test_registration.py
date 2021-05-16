fd7a9d1c219d736cb5da0f1c69db3f41ee4c45a6
Brian Icochea <ibrian93@gmail.com>
2021-05-16 19:40:39 +0200

    Invalid password criteria testing

--- Backend/Tests/test_registration.py
+++ Backend/Tests/test_registration.py
@@ -38,3 +38,20 @@ class TestClass:
         assert req.json()["code"] == "1200", "The code returned was: " + req.json()["code"]
         assert req.json()["message"] == "UserName and Password required.", "The message returned was: " + req.json()["message"]
     
+    def test_post_user_invalid_data_missing_username(self):
+        user_data = User().random_user()
+        data = Payload(username=None, password=user_data.password).to_dict()
+        req = self.account_service.post_user(data)
+        assert req.status_code == 400
+        assert req.json() is not None
+        assert req.json()["code"] == "1200", "The code returned was: " + req.json()["code"]
+        assert req.json()["message"] == "UserName and Password required.", "The message returned was: " + req.json()["message"]
+
+    def test_post_user_invalid_data_invalid_password_criteria_only_numbers(self):
+        user_data = User().random_user()
+        data = Payload(username=user_data.username, password="1234").to_dict()
+        req = self.account_service.post_user(data)
+        assert req.status_code == 400
+        assert req.json() is not None
+        assert req.json()["code"] == "1300", "The code returned was: " + req.json()["code"]
+        assert req.json()["message"] == "Passwords must have at least one non alphanumeric character, one digit ('0'-'9'), one uppercase ('A'-'Z'), one lowercase ('a'-'z'), one special character and Password must be eight characters or longer.", "The message returned was: " + req.json()["message"]
