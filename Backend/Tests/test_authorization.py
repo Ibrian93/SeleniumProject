9897a7a3d1a7280c3e9a4c0dc1e257ded890e333
Brian Icochea <ibrian93@gmail.com>
2021-06-05 18:56:51 +0200

    adding tests for authorization

--- Backend/Tests/test_authorization.py
+++ Backend/Tests/test_authorization.py
@@ -15,6 +15,17 @@ class TestClass:
         assert isinstance(req.json(), bool)
         assert req.json() is True
 
+    def test_post_authorized_new_user_generate_token_should_be_authorized(self, random_user):
+        data = Payload(username=random_user.username, password=random_user.password).to_dict()
+        req_generate_token = self.account_service.post_generate_token(data)
+        assert req_generate_token.status_code == 200
+        req = self.account_service.post_authorized(data)
+        assert req.status_code == 200
+        assert req.json() is not None
+        assert isinstance(req.json(), bool)
+        assert req.json() is True
+
+
     def test_post_authorized_wrong_password(self):
         data = Payload(username="ibrian93", password="MyTesting83?").to_dict()
         req = self.account_service.post_authorized(data)
@@ -48,6 +59,25 @@ class TestClass:
         assert req.json() is not None
         assert req.json()["code"] == "1207", "The code returned was: " + req.json()["code"]
         assert req.json()["message"] == "User not found!", "The message returned was: " + req.json()["message"]
+
+    def test_post_authorized_user_null_body(self):
+        req = self.account_service.post_authorized(data=None)
+        assert req.status_code == 400
+        assert req.json() is not None
+
+    def test_post_authorized_user_with_invalid_body_empty_list(self):
+        req = self.account_service.post_authorized(data=[])
+        assert req.status_code == 400
+        assert req.json() is not None
+        assert req.json()["code"] == "1200", "The code returned was: " + req.json()["code"]
+        assert req.json()["message"] == "UserName and Password required.", "The message returned was: " + req.json()["message"]
+
+    def test_post_authorized_user_with_invalid_body_invalid_json(self):
+        req = self.account_service.post_authorized(data="[]")
+        assert req.status_code == 400
+        assert req.json() is not None
+        assert req.json()["code"] == "1200", "The code returned was: " + req.json()["code"]
+        assert req.json()["message"] == "UserName and Password required.", "The message returned was: " + req.json()["message"]
 
     def test_post_created_user_but_no_generated_token(self, random_user):
         data = Payload(username=random_user.username, password=random_user.password).to_dict()
