1d4ebc679b39566e0138a043eb602661a6e78a4a
Brian Icochea <ibrian93@gmail.com>
2021-06-06 21:44:05 +0200

    Fixing bug: additional string was not concatenated with random user pass

--- Backend/conftest.py
+++ Backend/conftest.py
@@ -7,6 +7,7 @@ from Backend.Models.auth_payload import Payload
 @pytest.fixture
 def random_user():
     random_user = User().random_user()
-    data = Payload(username=random_user.username, password=random_user.password + "MyTesting83!").to_dict()
+    random_user.password = random_user.password + "MyTesting83!"
+    data = Payload(username=random_user.username, password=random_user.password).to_dict()
     req = Account().post_user(data)
     return random_user, req.json()
