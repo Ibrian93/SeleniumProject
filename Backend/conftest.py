f2da7bd0a81f82f71ffa0870d02cee2beeec1336
Brian Icochea <ibrian93@gmail.com>
2021-06-06 21:40:01 +0200

    Changing password random user in order to match criterias

--- Backend/conftest.py
+++ Backend/conftest.py
@@ -7,6 +7,6 @@ from Backend.Models.auth_payload import Payload
 @pytest.fixture
 def random_user():
     random_user = User().random_user()
-    data = Payload(username=random_user.username, password=random_user.password).to_dict()
+    data = Payload(username=random_user.username, password=random_user.password + "MyTesting83!").to_dict()
     req = Account().post_user(data)
     return random_user, req.json()
