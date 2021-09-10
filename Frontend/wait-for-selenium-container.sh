4cbe6851dc7082a99281ef7f580575000fcc7e74
dev <dev@MCM-08120-Laptop.localdomain>
2021-09-10 15:01:58 +0200

    changing docker standalone version to docker hub

--- Frontend/wait-for-selenium-container.sh
+++ Frontend/wait-for-selenium-container.sh
@@ -5,7 +5,7 @@ set -e
 
 cmd="$@"
 
-while ! curl -sSL "http://selenium:4444/wd/hub/status" 2>&1 \
+while ! curl -sSL "http://selenium-hub:4444/wd/hub/status" 2>&1 \
 		        | jq -r '.value.ready' 2>&1 | grep "true" >/dev/null; do
     	echo 'Waiting for the Container'
 	    sleep 1
